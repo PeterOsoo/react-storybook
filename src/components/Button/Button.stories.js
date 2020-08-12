2d535c923df5841c34570a4195f8aeb956b6c593
Peter <Posoo617@gmail.com>
2020-08-12 17:50:54 +0300

    decorators

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,16 +1,36 @@
 import React from "react"
 import Button from "./Button"
+import Center from "../Center/Center"
 
 export default {
 	title: "Form/Button",
 	component: Button,
+
 	// second key value pairs is optional
 }
 
-export const Primary = () => <Button variant="primary">Primary</Button>
-export const Secondary = () => <Button variant="secondary">Secondary</Button>
-export const Success = () => <Button variant="success">Success</Button>
-export const Danger = () => <Button variant="danger">Danger</Button>
+export const Primary = () => (
+	<Center>
+		<Button variant="primary">Primary</Button>
+	</Center>
+)
+export const Secondary = () => (
+	<Center>
+		<Button variant="secondary">Secondary</Button>
+	</Center>
+)
+
+export const Success = () => (
+	<Center>
+		<Button variant="success">Success</Button>
+	</Center>
+)
+
+export const Danger = () => (
+	<Center>
+		<Button variant="danger">Danger</Button>
+	</Center>
+)
 
 // create and visualize UI components in isolation
 // story book playground for UI components
